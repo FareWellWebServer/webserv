61b8b12f54c2d2a577e3f9895d2a921b39c670c6
yeonseong <yeonseong.dev@gmail.com>
2023-01-17 00:46:26 +0900

    INIT: Logger class 뼈대 잡기

--- include/Logger.hpp
+++ include/Logger.hpp
@@ -5,6 +5,11 @@ class Logger {
  public:
   Logger(void);
   virtual ~Logger(void);
+  /// @brief logger.info("message") 같은 방식으로 로깅에 사용.
+  /// @param msg
+  void info(std::string msg);
+  void warn(std::string msg);
+  void error(std::string msg);
 };
 
 #endif
