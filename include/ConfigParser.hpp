187bfa546bc1afca320890a88e3fa33580fa8167
jinhokim <rlawlsgh8113@naver.com>
2023-01-19 23:31:43 +0900

    FEAT: ConfigParser 기본 구조 잡기

--- include/ConfigParser.hpp
+++ include/ConfigParser.hpp
@@ -1,8 +1,15 @@
 #ifndef CONFIGPARSER_HPP
 #define CONFIGPARSER_HPP
 
+#define RED "\033[31m"
+#define GREEN "\033[32m"
+#define YELLOW "\033[33m"
+#define BLUE "\033[34m"
+#define WHITE "\033[37m"
+
 #include <fstream>
 #include <iostream>
+#include <sstream>
 #include <vector>
 
 #include "ServerConfigInfo.hpp"
@@ -12,7 +19,12 @@ class ConfigParser {
   ConfigParser(const char* file_path);
   ~ConfigParser(void);
 
-  void parse(void);
+  void Parse(void);
+  ServerConfigInfo ParseServer(std::istringstream &iss);
+
+  void PrintVector(std::vector<std::string> vec);
+  void Check(std::size_t pre, std::size_t cur, std::string key);
+  std::vector<std::string> split(std::string line, std::string charset);
 
  private:
   std::string content_;
--- src/ServerConfigInfo/ConfigParser.cpp
+++ src/ServerConfigInfo/ConfigParser.cpp
@@ -19,6 +19,88 @@ ConfigParser::ConfigParser(const char* file_path) {
 
 ConfigParser::~ConfigParser(void) {}
 
-void ConfigParser::parse(void) {
+void ConfigParser::Parse(void) {
   std::cout << "config parsing start" << std::endl;
+
+  if (content_.size() == 0)
+    throw std::runtime_error("[Config Error] wrong config syntax");
+
+  std::istringstream iss(content_);
+  std::string line;
+
+  while (42) {
+    std::getline(iss, line, '\n');
+    std::cout << "line: " << line << std::endl;
+    std::vector<std::string> vec = split(line, " \t\n");
+
+    if (vec.size() != 2 || vec[0] != "server" || vec[1] != "{")
+      throw std::runtime_error("[Config Error] wrong config syntax");
+    ServerConfigInfo serverConfigInfo = ParseServer(iss);
+    ServerConfigInfos_.push_back(serverConfigInfo);
+    break;
+  }
+  std::cout << "config parsing finish" << std::endl;
+}
+
+ServerConfigInfo ConfigParser::ParseServer(std::istringstream& iss) {
+  ServerConfigInfo serverConfigInfo;
+  std::string line;
+
+  while (42) {
+    std::getline(iss, line, '\n');
+    std::cout << "line: " << line << std::endl;
+
+    if (!line.size())
+      throw std::runtime_error("[Config Error] wrong config syntax");
+
+    std::vector<std::string> vec = split(line, " \t\n");
+    if (vec.size() == 1 && vec[0] == "}") break;
+  }
+
+  return serverConfigInfo;
+}
+
+void ConfigParser::Check(std::size_t pre, std::size_t cur, std::string key) {
+  if (pre != std::string::npos)
+    std::cout << "pre: " << pre << " -> " << content_[pre] << std::endl;
+  if (cur != std::string::npos)
+    std::cout << "cur: " << cur << " -> " << content_[cur] << std::endl;
+  if (key.size()) std::cout << "key: " << key << std::endl;
+  std::cout << std::endl;
+}
+
+std::vector<std::string> ConfigParser::split(std::string str,
+                                             std::string charset) {
+  std::vector<std::string> res;
+  std::string s;
+  size_t start = str.find_first_not_of(charset);
+  size_t end;
+  str += charset[0];
+
+  while (42) {
+    end = str.find_first_of(charset, start);
+    if (end == std::string::npos) {
+      break;
+    }
+    s = str.substr(start, end - start);
+    res.push_back(s);
+    start = str.find_first_not_of(charset, end);
+    if (start == std::string::npos) {
+      break;
+    }
+  }
+  PrintVector(res);
+  return res;
+}
+
+void ConfigParser::PrintVector(std::vector<std::string> vec) {
+  std::vector<std::string>::iterator start;
+  std::vector<std::string>::iterator it;
+  std::cout << "split result: ";
+  for (it = vec.begin(); it != vec.end(); ++it) {
+    std::cout << RED << *it;
+    if (std::distance(vec.begin(), it) != vec.size() - 1)
+      std::cout << WHITE << ", ";
+  }
+  std::cout << WHITE << std::endl;
 }
