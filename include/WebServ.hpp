a92e28200a89cabdf08923fc784cc26b7379d0f1
Sihun Lee <silee@c1r1s3.42seoul.kr>
2023-01-26 14:57:43 +0900

    FEAT: ReqHandler class생성

--- include/WebServ.hpp
+++ include/WebServ.hpp
@@ -2,9 +2,13 @@
 #define WEBSERV_HPP
 
 #include <iostream>
+#include <string>
+#include <iostream>
+#include <map>
 
 #include "ErrorHandler.hpp"
 #include "Logger.hpp"
 #include "WebServException.hpp"
+#include "ReqHandler.hpp"
 
 #endif
---
+++ src/ReqHandler/ReqHandler.cpp
@@ -0,0 +1,13 @@
+#include "../../include/Webserv.hpp"
+
+
+
+ReqHandler::ReqHandler(void)
+{
+}
+
+ReqHandler::~ReqHandler(void)
+{
+}
+
+
