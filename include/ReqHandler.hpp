ef624893e53f3f6da567f044d3020ba717203ce4
Sihun Lee <silee@c1r1s3.42seoul.kr>
2023-01-26 14:58:04 +0900

    FEAT: ReqHandler class생성

---
+++ include/ReqHandler.hpp
@@ -0,0 +1,29 @@
+#ifndef ReqHandler_HPP
+# define ReqHandler_HPP
+
+#include "WebServ.hpp"
+
+typedef struct t_req_msg{
+    std::string method;
+    std::string req_url;
+    std::string protocol;
+    std::map<std::string, std::string> headers;
+    bool        is_body;
+    std::string req_body;
+} s_req_msg;
+
+class ReqHandler
+{
+private :
+
+public :
+    s_req_msg req_msg;
+
+
+    ReqHandler(void);
+    ~ReqHandler(void);
+};
+
+
+
+#endif
