355a1c1086fdfa78ea96aab81a9de180a1b45751
haryu <haryu@student.42seoul.kr>
2023-01-29 21:36:22 +0900

    FEAT : make Method Processor Class & GET, HEAD Method
    - Method Processor 분기문 수정, 및 처리 완료
    - GET 헤더에서 CGI 를 제외한 처리 완료
    - HEAD 헤더 작업 완료

--- include/MethodProcessor.hpp
+++ include/MethodProcessor.hpp
@@ -24,10 +24,32 @@
 #define BOLDCYAN "\033[1m\033[36m"
 #define BOLDWHITE "\033[1m\033[37m"
 
+struct HTTPMessage {
+  /// @brief first line data : method, uri
+  std::string method;
+  std::string uri;
+  /// @brief each headers, 필수 정의 내린 해더들은 여기에 들어가야 하며,
+  /// 그외에는 버림
+  // host, UserAgent, Accept <- client
+  // server, set-cookie, etag... <- server
+  std::map<std::string, std::string> req_res_header_;
+  // connection, date <- client
+  // connection, date, keep-alive(time out, max) <- server
+  std::map<std::string, std::string> general_header_;
+  // content-type, content-length <- client
+  // content-type, content-length <- server
+  std::map<std::string, std::string> representation_header_;
+};
+
+struct entity {
+  char *entity_data_;
+  size_t entity_length_;
+};
+
 struct location {
-  std::string uri;                     // 없을 수가 없는 변수
-  int status_code;                     // 필수 입력 O
-  std::vector<std::string> file_path;  // 필수 입력 O
+  std::string uri;        // 없을 수가 없는 변수
+  int status_code;        // 필수 입력 O
+  std::string file_path;  // 필수 입력 O
 
   std::vector<std::string> methods;  // 필수 입력 X
 
@@ -40,31 +62,26 @@ struct location {
 };
 
 class ServerConfigInfo {
-  // private가 없는 이유
-  // 값 저장하려면 변수 별로 setter 함수를 만들어야 되서 일단 public으로
-  // 진행했습니다
  public:
   ServerConfigInfo(void);
   ~ServerConfigInfo(void);
 
   /* ======================== Variable ======================== */
-  std::string host;       // default -> 127.0.0.1
-  int port;               // 필수 입력 O
-  int body_size;          // 필수 입력 O
-  std::string root_path;  // 필수 입력 O
+  std::string host;         // default -> 127.0.0.1
+  int port;                 // 필수 입력 O
+  int body_size;            // 필수 입력 O
+  std::string root_path;    // 필수 입력 O
+  std::string file_path;    // 필수 입력 O
+  std::string upload_path;  // 필수 입력 O
 
   std::string server_name;  // 필수 입력 X
-
-  // 이름 변경된 변수 -> directory_list -> autoindex
-  bool autoindex;  // 필수 입력 X
-
-  // 논의 필요한 변수 keep_alive_time
-  // default를 정해준다고 치면 필수는 아닌데 정의는 되어야함
-  int timeout;  // 필수 입력 X
+  bool directory_list;      // 필수 입력 X
+  int timeout;              // 필수 입력 X
 
   std::vector<std::string> methods;        // 필수 입력 O
   std::map<int, std::string> error_pages;  // 필수 입력 O
-  std::vector<location> locations;         // 필수 입력 X
+
+  std::vector<location> locations;  // 필수 입력 X
 };
 
 class Config {
@@ -72,62 +89,66 @@ class Config {
   Config(const char *file_path);
   ~Config(void);
 
-  /* ======================== Utils ======================== */
-  // Error
-  void ExitConfigParseError(std::string msg = "") const;
-  void ExitConfigValidateError(const std::string &msg) const;
-  // Init
-  void InitServerConfigInfo(ServerConfigInfo &info);
-  void InitLocation(location &l, const std::string &uri);
-  // Parsing
-  bool IsNumber(const std::string &str) const;
-  bool IsWhiteLine(void) const;
-  bool IsOpenServerBracket(void) const;
-  bool IsOpenLocationBracket(const std::vector<std::string> &vec) const;
-  bool IsCloseBracket(const std::vector<std::string> &vec) const;
-  std::vector<std::string> Split(const std::string &str,
-                                 const std::string &charset,
-                                 int once = 0) const;
-  // Print
-  void Print(const std::string &color, const std::string &str,
-             int reset = 0) const;
-  void PrintKeyVal(const std::string &key, const std::string &val) const;
+  typedef std::vector<std::string> value;
 
-  void PrintConfigInfos(void) const;
-  void PrintConfigInfo(const ServerConfigInfo &info) const;
-  void PrintLocations(const std::vector<location> &locations) const;
-  void PrintLocation(const location &l) const;
+  /* ======================== Getter ======================== */
+  std::vector<ServerConfigInfo> GetServerConfigInfos(void);
 
   /* ======================== Parsing Server ======================== */
+  void InitServerConfigInfo(ServerConfigInfo &info);
+
   void Parse(int print_mode = 0);
   void ParseServer(void);
   void SetServerConfigInfo(const std::string &key, const std::string &val);
 
-  void ParseListen(const std::vector<std::string> &vec);
-  void ParseBodySize(const std::vector<std::string> &vec);
-  void ParseRoot(const std::vector<std::string> &vec);
-  void ParseServerName(const std::vector<std::string> &vec);
-  void ParseAutoindex(const std::vector<std::string> &vec);
-  void ParseTimeout(const std::vector<std::string> &vec);
-  void ParseMethods(const std::vector<std::string> &vec);
-  void ParseErrorPage(const std::vector<std::string> &vec);
+  void ParseListen(const value &vec);
+  void ParseBodySize(const value &vec);
+  void ParseRoot(const value &vec);
+  void ParseFilePath(const value &vec);
+  void ParseUploadPath(const value &vec);
+  void ParseServerName(const value &vec);
+  void ParseDirectoryList(const value &vec);
+  void ParseTimeout(const value &vec);
+  void ParseMethods(const value &vec);
+  void ParseErrorPage(const value &vec);
 
   /* ======================== Parsing Location ======================== */
+  void InitLocation(location &l, const std::string &uri);
+
   void ParseLocation(const std::string &key, const std::string &val);
   void SetLocation(location &l, const std::string &key, const std::string &val);
 
-  void ParseLocationStatusCode(location &l,
-                               const std::vector<std::string> &vec);
-  void ParseLocationRedirection(location &l,
-                                const std::vector<std::string> &vec);
-  void ParseLocationMethods(location &l, const std::vector<std::string> &vec);
-  void ParseLocationFilePath(location &l, const std::vector<std::string> &vec);
-  void ParseLocationCgi(location &l, const std::vector<std::string> &vec);
+  void ParseLocationStatusCode(location &l, const value &vec);
+  void ParseLocationRedirection(location &l, const value &vec);
+  void ParseLocationMethods(location &l, const value &vec);
+  void ParseLocationFilePath(location &l, const value &vec);
+  void ParseLocationCgi(location &l, const value &vec);
 
   /* ======================== Validation ======================== */
   bool CheckDuplicatePort(int port) const;
   void CheckValidation(void) const;
   void CheckLocation(const location &l) const;
+
+  /* ======================== Utils ======================== */
+  // Error
+  void ExitConfigParseError(std::string msg = "") const;
+  void ExitConfigValidateError(const std::string &msg) const;
+  // Parsing
+  bool IsNumber(const std::string &str) const;
+  bool IsWhiteLine(void) const;
+  bool IsOpenServerBracket(void) const;
+  bool IsOpenLocationBracket(const value &vec) const;
+  bool IsCloseBracket(const value &vec) const;
+  value Split(const std::string &str, const std::string &charset,
+              int once = 0) const;
+  // Print
+  void Print(const std::string &color, const std::string &str,
+             int reset = 0) const;
+  void PrintKeyVal(const std::string &key, const std::string &val) const;
+  void PrintConfigInfos(void) const;
+  void PrintConfigInfo(const ServerConfigInfo &info) const;
+  void PrintLocations(const std::vector<location> &locations) const;
+  void PrintLocation(const location &l) const;
 
  private:
   int print_mode_;
@@ -159,7 +180,7 @@ struct Data  // struct로
   struct HTTPMessage *req_message_;  // HTTP 요청/응답 헤더 구분 어떻게?
   struct HTTPMessage *res_message_;  // 같은 클래스로? 다른 클래스로?
   int status_code_;                  // 상태코드 enum 정의 필요
-  const struct entity_t *entity_;    // 응답 본문
+  struct entity *entity_;            // 응답 본문
 } Data;
 
 /**
@@ -196,7 +217,7 @@ class ClientMetaData {
   void SetEntity(char *entitiy);
 
   // data 통채로 원할 때
-  struct Data &GetData(int current_fd_);
+  struct Data *GetData(int current_fd_);
   // core에서 요청 헤더 데이터 필요할 때
   struct HTTPMessage *GetReqHeader();
   // core에서 응답 헤더 데이터 필요할 때
@@ -216,87 +237,156 @@ class ClientMetaData {
   char *GetURL();
 };
 
-struct entity {
-  char *entity_;
-  size_t entity_length_;
-} entity_t;
+#define CGI ".py"
+#define INSTALLPATH "/original/path"
+#define INDEX "index.html"
 
-enum Method { DEFAULT, GET, HEAD, PUT, POST, DELETE };
+#include <unistd.h>
 
 class MethodProcessor {
  private:
-  bool isExistFile(std::string filePath_);
-  static std::map<int, struct entity_t *> cache_Entity_;
+  static std::map<int, struct entity *> cache_entity_;
+  void fetchOiginalPath(std::string &uri) {
+    uri.erase(0);
+    uri.insert(0, INSTALLPATH);
+    return;
+  }
+  bool isExistFile(std::string &uri) {
+    const char *temp = uri.c_str();
 
-  struct entity_t *MethodGET(std::string filePath_) {
-    struct entity_t *ret;
+    int ret = access(temp, F_OK);
+    delete[] temp;
 
-    // 들어오는 URI 방식
-    // default page => / (root)
-    // default page 의 절대 경로를 알아야 한다.
-    // 들어오는 그 다음 페이지는 => /src/~~ 이런식으로 붙음. 즉, uri 에서 핵심은
-    // default 로 들어오는 루트 구별, 거기서 핵심은 포트일듯, 이후 이를 다
-    // 합쳐서 구분하는 것이 핵심이다.
-    // 즉 단순 client meta data 뿐만 아니라 서버 config 도 들어와서 이를 동시에
-    // 확인해야함.
+    if (ret != 0) return (false);
+    return (true);
+  }
+  bool isCgi(std::string &uri) {
+    std::string::reverse_iterator it = uri.rbegin();
+    if (it[0] == CGI[2]) {
+      if (it[1] == CGI[1]) {
+        if (it[2] == CGI[0]) return (true);
+      }
+    } else
+      return (false);
+  }
+  bool isHtml(std::string &uri, const char *identifier) {
+    std::string::reverse_iterator it = uri.rbegin();
 
-    /* 전체 로직 정리 */
-    // 파일 존재 여부 확인
+    size_t len = strlen(identifier);
 
-    // NO : 404 file notfound?
-    //// entity char * = NULL
-    //// entity_length_ = -1;
-    //// error code  = setting like HTTP
+    for (size_t i = 0; i < len; i++) {
+      if (it[i] == identifier[len - i])
+        continue;
+      else
+        return (false);
+    }
+    return (true);
+  }
+  void MethodGETCgi(struct Data *client) {
+    // fork
+    // 자식
+    /// 파이프 작업(표준 입출력 -> 파이프 입력, 출력은 삭제
+    //// CGI 실행
+    // 부모
+    /// 파이프 작업(파이프 입력 닫기
+    /// wait(??)
+    //// wait 풀림, 데이터 받기 => CGI 데이터는 어차피 표준 입출력으로 구성 되어
+    /// 있을 것이므로 buf에 받고 string 으로 넣어도 상관 없음 / cstr() 으로 파일
+    /// 만들어서 client 등록
+  }
+  void MethodGETFile(struct Data *client) {
+    struct entity *ret;
+    ret = new struct entity();
+    std::ifstream entityFile;
 
-    // YES : std::map <std::string, struct entity_t *> 에 등록된 URI인지 확인.
+    entityFile.open(client->req_message_->uri, std::ifstream::in);
+    entityFile.seekg(0, entityFile.end);
+    ret->entity_length_ = entityFile.tellg();
+    entityFile.seekg(0, entityFile.beg);
 
-    //// Hit : 해당 데이터 만 옮기고 리턴
+    ret->entity_data_ = new char[ret->entity_length_ + 1];
+    ret->entity_data_[ret->entity_length_] = '\0';
 
-    //// miss : 해당 데이터 찾기 로직으로 들어감
-    ////// 파일 열기
-    ////// 파일 읽기 로직 진행 + content length 넣기
-    ////// 일반적인 index.html 문서의 경우 이에 대한 캐싱 작업(std::map.insert)
-    ////// 반환
+    entityFile.read(ret->entity_data_, ret->entity_length_);
 
-    return (ret);
+    client->entity_->entity_data_ = ret->entity_data_;
+    return;
+  }
+  void MethodGET(struct Data *client) {
+    struct entity *ret;
+
+    fetchOiginalPath(client->req_message_->uri);
+    if (!isExistFile(client->req_message_->uri)) {
+      client->status_code_ = 404;
+      client->entity_->entity_data_ = NULL;
+      client->entity_->entity_length_ = 0;
+      return;
+    }
+    if (isCgi(client->req_message_->uri)) {
+      MethodGETCgi(client);
+      return;
+    }
+    if (!isHtml(client->req_message_->uri, INDEX)) {
+      MethodGETFile(client);
+      return;
+    }
+    std::map<int, struct entity *>::iterator check_cache =
+        cache_entity_.find(client->port_);
+    if (check_cache != cache_entity_.end()) {
+      client->entity_->entity_data_ =
+          static_cast<std::map<int, struct entity *>>(cache_entity_)
+              .at(0)
+              ->entity_data_;
+      client->entity_->entity_length_ =
+          static_cast<std::map<int, struct entity *>>(cache_entity_)
+              .at(0)
+              ->entity_length_;
+      return;
+    }
+
+    ret = new struct entity();
+    std::ifstream entityFile;
+
+    entityFile.open(client->req_message_->uri, std::ifstream::in);
+    entityFile.seekg(0, entityFile.end);
+    ret->entity_length_ = entityFile.tellg();
+    entityFile.seekg(0, entityFile.beg);
+
+    ret->entity_data_ = new char[ret->entity_length_ + 1];
+    ret->entity_data_[ret->entity_length_] = '\0';
+
+    entityFile.read(ret->entity_data_, ret->entity_length_);
+
+    client->entity_->entity_data_ = ret->entity_data_;
+    cache_entity_.emplace(client->port_, ret);
+    return;
   }
   /// @brief GET 과 동일하게 진행하며, 단순 메서드로 구분한다.
-  struct entity_t *MethodHEAD(std::string filePath_);
-  void MethodPOST();
-  void MethodDELETE();
-  void MethodPUT();
+  void MethodHEAD(struct Data *client) {
+    MethodGET(client);
+    client->entity_->entity_data_ = NULL;
+    return;
+  }
+  void MethodPOST(struct Data *client);
+  void MethodPUT(struct Data *client);
+  void MethodDELETE(struct Data *client);
 
  public:
   MethodProcessor(void);
   ~MethodProcessor(void);
   void MethodProcessorInput(ClientMetaData *clients) {
-    // 요청 메서드 구분
-    // switch 로 구분하여 들어가야할 메서드 처리 함수 메서드로 진입
-    // 정리가 마무리 되면 그대로 전달 및 처리
-    Method switching = DEFAULT;
-
-    switch (switching) {
-      case GET: {
-        /* Method 호출 */
-        break;
-      }
-      case HEAD: {
-        /* Method 호출 */
-        break;
-      }
-      case POST: {
-        /* Method 호출 */
-        break;
-      }
-      case PUT: {
-        /* Method 호출 */
-        break;
-      }
-      case DELETE: {
-        /* Method 호출 */
-        break;
-      }
-    }
+    struct Data *client;
+    client = clients->GetData(clients->GetPort());
+    if (client->req_message_->method == "GET")
+      MethodGET(client);
+    else if (client->req_message_->method == "HEAD")
+      MethodHEAD(client);
+    else if (client->req_message_->method == "POST")
+      MethodPOST(client);
+    else if (client->req_message_->method == "PUT")
+      MethodPUT(client);
+    else
+      MethodDELETE(client);
   }
 };
 #endif
