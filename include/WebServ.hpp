d5a6225e896b89ced50c0c8db6396961d4be0433
YeonSeong-Lee <yeonseong.dev@gmail.com>
2023-02-02 17:36:37 +0900

    HOTFIX: color define to Color.hpp

--- include/WebServ.hpp
+++ include/WebServ.hpp
@@ -1,19 +1,7 @@
 #ifndef WEBSERV_HPP
 #define WEBSERV_HPP
 
-#define RED "\033[31m"
-#define GREEN "\033[32m"
-#define YELLOW "\033[33m"
-#define BLUE "\033[34m"
-#define WHITE "\033[37m"
-#define RESET "\033[0m"
-#define BOLDRED "\033[1m\033[31m"
-#define BOLDGREEN "\033[1m\033[32m"
-#define BOLDYELLOW "\033[1m\033[33m"
-#define BOLDBLUE "\033[1m\033[34m"
-#define BOLDMAGENTA "\033[1m\033[35m"
-#define BOLDCYAN "\033[1m\033[36m"
-#define BOLDWHITE "\033[1m\033[37m"
+
 
 #define BUFFER_SIZE 1024
 
@@ -26,6 +14,7 @@
 #include <map>
 #include <string>
 
+#include "Color.hpp"
 #include "HTTPMassage.hpp"
 #include "ClientMetaData.hpp"
 #include "ServerConfigInfo.hpp"
