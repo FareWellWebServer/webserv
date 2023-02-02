23469d0905affcb6a51976b5b7a9d2fd9fbaec36
Dong_D <ehdgud6634@gmail.com>
2023-02-02 22:30:24 +0900

    WIP: make MetaData Getter and Setter

--- include/Data.hpp
+++ include/Data.hpp
@@ -1,33 +1,98 @@
 #ifndef DATA_HPP
 #define DATA_HPP
 
-enum stage {
-  READY,
-  REQ_READY,
-  REQ_FINISHED, 
-  GET_READY,
-  GET_CGI, 
-  GET_FINISHED,
-  POST_READY, 
-  POST_PROCESSING,
-  POST_
-};
+#include "HTTPMassage.hpp"
+#include "ServerConfigInfo.hpp"
+#include <string>
 
-/* 포인터로 갖고있는 녀석들의 메모리 관리는 어디서 해줄지? */
+#define NULL 0
+#define READ 0
+#define WRITE 1
+
+// enum stage {
+//   READY,
+//   REQ_READY,
+//   REQ_FINISHED, 
+//   GET_READY,
+//   GET_CGI, 
+//   GET_FINISHED,
+//   POST_READY, 
+//   POST_PROCESSING,
+//   POST_
+// };
+
+/* 포인터로 갖고있는 녀석들의 메모리 관리는 어디서 해줄지? 
+  객체 자신에서 안해주는거는 Data 에서 해주기
+*/
+
+/**
+ * @brief Data 에서는 Get만 가능. Set은 Handler에서
+ * 
+ */
+
 class Data {
- public:
-  Data();
-  ~Data();
-  int litsen_fd_;  // 어느 listen fd에 연결됐는지
-  int port_;  // listen fd에 bind 되어있는 port 번호. config볼 때 필요
-  // int client_fd_;
-  struct kevent* event_;  // fd(ident), flag들
-  // ServerConfigInfo* config_;
-  // struct HTTPMessage* req_message_;  // HTTP 요청/응답 헤더 구분 어떻게?
-  // struct HTTPMessage* res_message_;  // 같은 클래스로? 다른 클래스로?
-  int status_code_;  // 상태코드 enum 정의 필요
-  char* entity_;     // 응답 본문
-  stage e_stage;
+  public:
+    Data();
+    ~Data();
+    void Clear();
+    int GetListenFd() const;
+    int GetListenPort() const;
+    int GetClientFd() const; //kevent
+    int GetStatusCode() const;
+    bool IsTimeout() const;
+    int GetFileFd() const;
+    int GetPipeWrite() const;
+    int GetPipeRead() const;
+
+    /* Get Config */
+    // int GetConfig() const; // Config에서 어떤 정보를 뭘 보고 반환해줘야하는지?
+
+    /* Get about Request Massage */
+    t_req_msg* GetReqMassage() const;
+    std::string GetReqMethod() const;
+    std::string GetReqURL() const;
+    std::string GetReqProtocol() const;
+    std::string GetReqHeaderByKey(std::string &key) const;
+    char* GetReqBodyData() const;
+    char* GetReqBodyType() const;
+    size_t GetReqBodyLength() const;
+
+    /* Get about Response Massage */
+    t_res_msg* GetResMassage() const;
+    std::string GetResVersion() const;
+    int GetResStatusCode() const;
+    std::string GetResStatusText() const;
+    std::string GetResHeaderByKey(std::string &key) const;
+    char* GetResBodyData() const;
+    char* GetResBodyType() const;
+    size_t GetResBodyLength() const;
+    void SetResMassage(t_res_msg* res_msg);
+    void SetResEntity(t_entity* entity);
+    t_entity* GetResEntity() const;
+    
+
+
+  public:
+    int litsen_fd_;  // 어느 listen fd에 연결됐는지
+    int port_;  // listen fd에 bind 되어있는 port 번호. config볼 때 필요
+    int client_fd_;
+    int status_code_;  // 상태코드 enum 정의 필요
+    bool timeout_;
+    bool cgi;
+    int file_fd_;
+    int pipe_[2];
+    struct kevent* event_;  // fd(ident), flag들
+    const ServerConfigInfo* config_;
+    t_req_msg* req_message_;
+    t_res_msg* res_message_; // ResHandler 에게 보내줄 내용 정리
+    t_entity* res_entity_; // method가 넣어주는 곳
+    // stage e_stage;
 };
 
 #endif
+
+
+
+
+
+//TODO: timeout setting 어떻게? SetTimeOut(bool)
--- src/MetaData/Data.cpp
+++ src/MetaData/Data.cpp
@@ -1,16 +1,159 @@
 #include "../../include/Data.hpp"
-#define NULL 0
 
-Data::Data() {
-  litsen_fd_ = -1;
-  port_ = -1;
-  // client_fd_ = -1;
-  event_ = NULL;
-  // config_ = NULL;
-  // req_message_ = NULL;
-  // res_message_ = NULL;
-  status_code_ = 200;
-  entity_ = NULL;
+/**
+ * @brief 세팅 후 연결 해제까지 바뀌지 않아야하는 값은 생성자에서 초기화
+ * 
+ */
+Data::Data() : \
+litsen_fd_(-1), port_(-1), client_fd_(-1), config_(NULL), timeout_(false), event_(NULL) {
+  req_message_ = NULL;
+  res_entity_ = NULL;
+  Clear();
 }
 
-Data::~Data() {}
+Data::~Data() {
+  Clear();
+}
+
+/**
+ * @brief 새로운 요청 들어올때마다 초기화할 값
+ * delete : ReqMassage, res_entity_
+ */
+void Data::Clear() {
+  status_code_ = 200;
+  if (req_message_ != NULL) {
+    delete req_message_;
+    req_message_ = NULL;
+  }
+  if (res_entity_ != NULL) {
+    delete res_entity_;
+    res_entity_ = NULL;
+  }
+  if (res_message_)
+  cgi = false;
+  file_fd_ = -1;
+  pipe_[READ] = -1;
+  pipe_[WRITE] = -1;
+}
+
+int Data::GetListenFd() const {
+  return litsen_fd_;
+}
+
+int Data::GetListenPort() const {
+  return port_;
+}
+
+int Data::GetClientFd() const {
+  return client_fd_;
+}
+
+int Data::GetStatusCode() const {
+  return status_code_;
+}
+
+bool Data::IsTimeout() const {
+  return timeout_;
+}
+
+int Data::GetFileFd() const {
+  return file_fd_;
+}
+
+int Data::GetPipeWrite() const {
+  return pipe_[WRITE];
+}
+
+int Data::GetPipeRead() const {
+return pipe_[READ];
+}
+
+t_req_msg* Data::GetReqMassage() const {
+  return req_message_;
+}
+
+std::string Data::GetReqMethod() const {
+  return req_message_->method_;
+}
+
+std::string Data::GetReqURL() const {
+  return req_message_->req_url_;
+}
+
+std::string Data::GetReqProtocol() const {
+  return req_message_->protocol_;
+}
+
+/**
+ * @brief Request Header의 키값 넣고, 그에 해당하는 value 반환
+ * 
+ * @param key 
+ * @return std::string, 못찾으면 빈문자열
+ */
+std::string Data::GetReqHeaderByKey(std::string &key) const {
+  return req_message_->headers_[key];
+}
+
+char* Data::GetReqBodyData() const {
+  return req_message_->body_data_->data_;
+}
+
+char* Data::GetReqBodyType() const {
+  return req_message_->body_data_->type_;
+}
+
+size_t Data::GetReqBodyLength() const {
+  return req_message_->body_data_->length_;
+}
+
+
+/* Get about Response Massage */
+t_res_msg* Data::GetResMassage() const {
+  return res_message_;
+}
+
+std::string Data::GetResVersion() const {
+  return res_message_->http_version_;
+}
+
+int Data::GetResStatusCode() const {
+  return res_message_->status_code_;
+}
+
+std::string Data::GetResStatusText() const {
+  return res_message_->status_text_;
+}
+
+/**
+ * @brief Request Header의 키값 넣고, 그에 해당하는 value 반환
+ * 
+ * @param key 
+ * @return std::string, 못찾으면 빈문자열
+ */
+std::string Data::GetResHeaderByKey(std::string &key) const {
+  return res_message_->headers_[key];
+}
+
+char* Data::GetResBodyData() const {
+  return res_message_->body_data_->data_;
+}
+
+char* Data::GetResBodyType() const {
+  return res_message_->body_data_->type_;
+}
+
+size_t Data::GetResBodyLength() const {
+  return res_message_->body_data_->length_;
+}
+
+void Data::SetResMassage(t_res_msg* res_msg) {
+  res_message_ = res_msg;
+}
+
+void Data::SetResEntity(t_entity* entity) {
+  res_entity_ = entity;
+}
+
+t_entity* Data::GetResEntity() const {
+  return res_entity_;
+}
