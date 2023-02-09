70f739bf3417ca2fb4a4db6ce5ed2592fe710fb5
jinhokim <rlawlsgh8113@naver.com>
2023-02-09 20:54:35 +0900

    FEAT: kst 현재 시간 반환해주는 함수 추가

---
+++ src/Utils/time.cpp
@@ -0,0 +1,16 @@
+#include <ctime>
+#include <iostream>
+
+std::string GetCurrentDate(void) {
+  time_t raw_time;
+  struct tm* pm;
+  char format[40];
+
+  time(&raw_time);
+  pm = gmtime(&raw_time);
+  pm->tm_hour += 9;
+  pm->tm_mday += pm->tm_hour / 24;
+  pm->tm_hour %= 24;
+  strftime(format, 29, "%a, %d %b %G %X KST", pm);
+  return std::string(format);
+}
