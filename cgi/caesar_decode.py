2e2c1729790ace9691b58052aab3cf8d3d291b73
YeonSeong-Lee <yeonseong.dev@gmail.com>
2023-02-09 20:10:24 +0900

    FEAT: 카이사르 암호 복호화 추가

--- cgi/caesar_decode.py
+++ cgi/caesar_decode.py
@@ -4,9 +4,9 @@ def caesar(s):
     s = list(s)
     for i in range(len(s)):
         if s[i].isupper():
-            s[i]=chr((ord(s[i]) - ord('A')+ 42) % 26 + ord('A'))
+            s[i]=chr((ord(s[i]) - ord('A') - 42) % 26 + ord('A'))
         elif s[i].islower():
-            s[i]=chr((ord(s[i]) - ord('a')+ 42) % 26 + ord('a'))
+            s[i]=chr((ord(s[i]) - ord('a') - 42) % 26 + ord('a'))
  
     return "".join(s)
 
